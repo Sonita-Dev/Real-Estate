e940c776f6f7115a2e322a6a60c05266740d1f22
Sonita2000 <sonitaheangpretty@gmail.com>
2025-08-23 23:38:56 +0700

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { motion } from "framer-motion";
+import headerImg from "../assets/header_img.png";
+
 const Header = () => {
   return (
     <div
       className="min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden"
-      style={{ backgroundImage: "url('/header_img.png')" }}
+      style={{ backgroundImage: `url(${headerImg})` }}
       id="Header"
     >
       <Navbar />
