4fb79e5fed79aecfd2394827efa2f2dc642141f6
Sonita2000 <sonitaheangpretty@gmail.com>
2025-10-29 13:00:14 +0700

    Fix assets import path in Header.jsx to correct relative path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,16 @@
 import React from "react";
 import Navbar from "./Navbar";
-import { motion } from "framer-motion";
-import headerImg from "../assets/header_img.png";
+import { assets } from "../assets/assets/assets";
 
 const Header = () => {
   return (
     <div
       className="min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden"
-      style={{ backgroundImage: `url(${headerImg})` }}
+      style={{ backgroundImage: `url(${assets.header_img})` }}
       id="Header"
     >
       <Navbar />
-      <motion.div
-        initial={{ opacity: 0, y: 100 }}
-        transition={{ duration: 1.5 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
-        className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white"
-      >
+      <div className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white">
         <h2 className="text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20">
           Explore homes that fit your dreams
         </h2>
@@ -29,7 +22,7 @@ const Header = () => {
             Contact Us
           </a>
         </div>
-      </motion.div>
+      </div>
     </div>
   );
 };
